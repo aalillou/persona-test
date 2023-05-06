c36255aa946aebca65135946972ff6dd1015b447
Mo <mo@aalillou.be>
2023-05-06 07:14:06 +0200

    update master

--- src/main/java/be/ond/vlaanderen/persona/Main.java
+++ src/main/java/be/ond/vlaanderen/persona/Main.java
@@ -2,6 +2,6 @@ package be.ond.vlaanderen.persona;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Persona! 1");
+        System.out.println("Hello Persona! 1 3");
     }
 }
